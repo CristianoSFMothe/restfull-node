6f9323c3e63ee0e210b725c1ecb7c40b68d3640a
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-05-06 11:00:19 -0300

    :rocket: Utilizando o `consign`

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,24 @@
-let express = require('express');
-let routes = express.Router();
+module.exports = (app) => {
 
-
-routes.get('/', (req, res) => {
-    res.started = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: [{
-            name: 'Cristiano',
-            email: 'cristiano@gmail.com',
-            id: 1
-        }]
+    app.get('/users', (req,
+        res) => {
+        res.started = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [{
+                name: 'Cristiano',
+                email: 'cristiano@gmail.com',
+                id: 1
+            }]
+        });
     });
-});
 
-routes.get('/admin', (req, res) => {
-    res.started = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: []
+    app.get('/users/admin', (req,
+        res) => {
+        res.started = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: []
+        });
     });
-})
-
-module.exports = routes;
+};

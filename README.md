865b2ff8cf55aa1e5624cc0d4051bc8ebc7833b5
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-05-06 10:18:50 -0300

    :up: Adicionando o `Nodemon`

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # restfull-node
+
+## Iniciando 
+
+```bash
+node init
+```
+* Instando o Express
+
+```bash
+npm install express --save
+```
+* Instalando Nodemon
+
+```bash
+npm install nodemon -g
+```
  
--- index.js
+++ index.js
@@ -1,39 +1,27 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
+let app = express();
 
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
-
-    switch (req.url) {
-
-        case '/':
-
-            res.started = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Hello, world!</h1>');
-
-            break;
-
-        case '/users':
-
-            res.started = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                users: [{
-                    name: 'Cristiano',
-                    email: 'cristiano@gmail.com',
-                    id: 1
-                }]
-            }));
-
-            break;
-
-    }
-
+app.get('/', (req, res) => {
+    res.started = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello, world!</h1>');
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.get('/users', (req, res) => {
+    res.started = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: [{
+            name: 'Cristiano',
+            email: 'cristiano@gmail.com',
+            id: 1
+        }]
+    });
+});
+
+
+app.listen(3000, '127.0.0.1', () => {
 
     console.log('server started');
 });
